e8931dff91150fb2d85b0c34039bbb2c9abf64b0
poorya-git <76785950+poorya-git@users.noreply.github.com>
2020-12-31 13:27:30 +0400

    Update vitrin.m

--- vitrin.m
+++ vitrin.m
@@ -1,1 +1,1 @@
-{"تیتل":"تتلو","توضیح":"تتلو","لینک":"https://emroozia.com/wp-content/uploads/2020/10/tataoo-allah-emroozia-com-100799-1.jpg","لینک2":"https://uupload.ir/view/8el8_amir_tataloo_-_allah.mp3/"}
+[{"تیتل":"تتلو","توضیح":"تتلو","لینک":"https://emroozia.com/wp-content/uploads/2020/10/tataoo-allah-emroozia-com-100799-1.jpg","لینک2":"https://uupload.ir/view/8el8_amir_tataloo_-_allah.mp3/"}]
